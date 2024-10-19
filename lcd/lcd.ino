5c7b759ba855dcbfeb9c99cb7d248c8158794829
zdust115 <rickyatomic911@gmail.com>
2024-10-19 23:53:48 +0200

    lcd prove

---
+++ lcd/lcd.ino
@@ -0,0 +1,16 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>  // Usa la libreria LiquidCrystal
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);  // Indirizzo I2C e dimensione del display
+
+void setup() {
+  Wire.begin(D2, D1);  // SDA su D2, SCL su D1 (ESP8266)
+  lcd.begin(16, 2);    // Inizializza il display 16x2
+  lcd.backlight();     // Accendi la retroilluminazione
+  lcd.setCursor(0, 0); // Imposta il cursore sulla prima riga
+  lcd.print("Hello, world!");  // Mostra il testo
+}
+
+void loop() {
+  // Niente nel loop
+}
